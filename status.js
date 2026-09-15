3752b50a4efd62f8c66bdc2f0c9647a71f1049a2
komalasari2986 <komalasari2986@gmail.com>
2026-09-15 18:41:54 +0700

    status.js

--- status.js
+++ status.js
@@ -3,5 +3,5 @@
    - Ubah `IS_MAINTENANCE` ke `true` jika sedang perbaikan
    - Ubah `IS_MAINTENANCE` ke `false` jika berjalan normal
    ====================================================== */
-var IS_MAINTENANCE = false; // <-- Gunakan var agar terbaca di window global
+var IS_MAINTENANCE = true; // <-- Gunakan var agar terbaca di window global
 var MAINTENANCE_BYPASS_KEY = "admin123";
