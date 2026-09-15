4b1f2ede61278412b1027af04848e51c741993db
komalasari2986 <komalasari2986@gmail.com>
2026-09-15 18:21:54 +0700

    Add files via upload

---
+++ status.js
@@ -0,0 +1,7 @@
+/* ======================================================
+   KONTROL MODE MAINTENANCE / PEMELIHARAAN SISTEM
+   - Ubah `IS_MAINTENANCE` ke `true` jika sedang perbaikan
+   - Ubah `IS_MAINTENANCE` ke `false` jika berjalan normal
+   ====================================================== */
+const IS_MAINTENANCE = true; // <-- UBAH KE true / false DI SINI!
+const MAINTENANCE_BYPASS_KEY = "admin123"; // Password bypass developer
